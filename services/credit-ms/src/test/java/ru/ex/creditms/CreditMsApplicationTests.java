18d14d0406c5249c4cebfb73c7fe3d445f6ea5c0
Danil <dabcom2013@gmail.com>
2025-09-19 11:13:50 +0300

    credit-ms. Create credit microservice proj

---
+++ services/credit-ms/src/test/java/ru/ex/creditms/CreditMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ex.creditms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
