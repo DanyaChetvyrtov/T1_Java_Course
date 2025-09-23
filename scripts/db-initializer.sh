2a74556492a12b8682b635c071f06bc459e3bc4c
Danil <dabcom2013@gmail.com>
2025-09-23 11:41:10 +0300

    scripts. update db-initializer. Add account_db creation.

--- scripts/db-initializer.sh
+++ scripts/db-initializer.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "PostgreSQL is ready. Creating databases..."
 
-for cur_db in "client_db" "credit_db"; do
+for cur_db in "client_db" "credit_db" "account_db"; do
   psql -U "$POSTGRES_USER" -c "CREATE DATABASE ${cur_db};" || echo "Database '${cur_db}' already exists"
 done
